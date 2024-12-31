822ada2a80955db735f456e24d4bdfeaf3dada23
uyashaISHINI <uyashaishini@gmail.com>
2024-12-31 11:15:26 +0530

    insert logo into the login page😊

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -3,7 +3,13 @@ import { Link } from "react-router-dom";
 export default function LoginPage() {
     return (
         <div className="flex justify-center items-center w-full h-screen bg-slate-400">
-            <div className="w-[400px] h-[400px] bg-slate-600">
+            <div className="w-[400px] h-[400px] bg-slate-600 flex flex-col justify-center items-center">
+                <img src="/logo.png" className="rounded-full w-[100px]"/>
+                <span>Email</span>
+                <input/>
+                <span>Password</span>
+                <input type="password"/>
+                <button className="bg-white">Login</button>
             </div>
         </div>
     );
