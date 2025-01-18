a4f9b48327c1ac7258da296c536c9ba1f9ca2719
uyashaISHINI <uyashaishini@gmail.com>
2025-01-18 12:12:06 +0530

    Delete the test file

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import { useState } from 'react'
 import './App.css'
 import ProductCard from './components/productCard'
 import UserData from './components/UserData'
-import Testing from './components/testing'
 import LoginPage from './pages/loginPage'
 import HomePage from './pages/homePage'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AdminHomePage from './pages/adminHomePage'
 import SignUpPage from './pages/signUpPage'
 import { Toaster } from 'react-hot-toast'
-import FileUploadTest from './pages/test'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -26,7 +24,6 @@ function App() {
     <Route path = "/signUp" element={<SignUpPage/>}/>
     <Route path = "/admin/*" element={<AdminHomePage/>}/>
     <Route path ="/*"element={<HomePage/>}/>
-    <Route path="/testing" element={<FileUploadTest/>}/>
     </Routes>
     </BrowserRouter>       
     </>
