9df1099ffe4267e337392ec9f89104c937b5d0e0
uyashaISHINI <uyashaishini@gmail.com>
2025-01-22 20:33:27 +0530

    Update the product card cord

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -4,10 +4,13 @@ export default function ProductCard(props){
     console.log(props)
     return(
             <Link to={`/productInfo/${props.product.productId}`}>
-            <div className='w-[300px] h-[400px] m-[10px] rounded-xl shadow-lg shadow-gray-500 hover:shadow-primary hover:border hover:border-gray-500 flex flex-col items-center justify-center'>
-                <img src={props.product.images[0]} alt="" className='h-[200px] w-[200px] object-cover'/>
-                <div className="text-lg font-semibold">{props.product.name}</div>
-                <div className="text-lg font-semibold">LKR {props.product.price}</div>
+            <div className='w-[300px] h-[400px] m-[10px] rounded-xl shadow-lg shadow-gray-500 hover:shadow-primary hover:border-[3px] overflow-hidden flex flex-col'>
+                <img src={props.product.images[0]} alt="" className='h-[65%] w-full object-cover '/>
+                <div className="max-h-[35%] h-[35%] p-4 flex flex-col justify-between">
+                <h1 className="text-3xl font-bold text-secondary text-center">{props.product.productName}</h1>
+                <p className="text-left text-xl font-semibold">LKR. {props.product.lastPrice.toFixed(2)}</p>
+                <p className="text-left text-lg text-gray-500  font-semibold line-through">LKR. {props.product.price.toFixed(2)}</p>
+                </div>
             </div>
             </Link>
     )
