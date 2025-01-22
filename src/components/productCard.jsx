aecf6c0ced92269150f0adbb08b2877a80280c99
uyashaISHINI <uyashaishini@gmail.com>
2025-01-22 20:05:12 +0530

    Added a product card

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,9 +1,14 @@
-export default function ProductCard(){
+import { Link } from "react-router-dom"
+
+export default function ProductCard(props){
+    console.log(props)
     return(
-        <div>
-            <h1>Product one</h1>
-            <h2>Price : $100</h2>
-            <button>Add to cart</button>
-        </div>
+            <Link to={`/productInfo/${props.product.productId}`}>
+            <div className='w-[300px] h-[400px] m-[10px] rounded-xl shadow-lg shadow-gray-500 hover:shadow-primary hover:border hover:border-gray-500 flex flex-col items-center justify-center'>
+                <img src={props.product.images[0]} alt="" className='h-[200px] w-[200px] object-cover'/>
+                <div className="text-lg font-semibold">{props.product.name}</div>
+                <div className="text-lg font-semibold">LKR {props.product.price}</div>
+            </div>
+            </Link>
     )
 }
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -2,6 +2,7 @@ import { Link, Route, Routes } from 'react-router-dom';
 import Header from '../components/header';
 import LoginPage from './loginPage';
 import ProductOverview from './home/productOverview';
+import ProductPage from './home/product';
 
 const HomePage = () => {
     return (
@@ -10,6 +11,7 @@ const HomePage = () => {
             <div className='w-full h-[calc(100vh-100px)]'>
                 <Routes>
                     <Route path="/" element={<h1>Home Page</h1>} />
+                    <Route path="/products" element={<ProductPage/>}/>
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/productInfo/:id" element={<ProductOverview />} />
                 </Routes>
