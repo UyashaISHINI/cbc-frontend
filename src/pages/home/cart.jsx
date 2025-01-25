78281eaafe8b6254848b176cefc7870eaec92499
uyashaISHINI <uyashaishini@gmail.com>
2025-01-25 14:00:17 +0530

    debugging the cart Card😊

--- src/pages/home/cart.jsx
+++ src/pages/home/cart.jsx
@@ -59,10 +59,11 @@ export default function Cart(){
                         )
                     }
                 )
-            }</table>
-            <h1 className="text-3xl font-bold text-secondary">Total: LKR. {labeledTotal.toFixed(2)}</h1>
-            <h1 className="text-3xl font-bold text-secondary">Discount: LKR. {(labeledTotal-total).toFixed(2)}</h1>
-            <h1 className="text-3xl font-bold text-secondary">Grand Total: LKR. {total.toFixed(2)}</h1>
+            }
+            </table>
+            <h1 className="text-2xl font-bold text-secondary">Total: LKR. {labeledTotal.toFixed(2)}</h1>
+            <h1 className="text-2xl font-bold text-secondary">Discount: LKR. {(labeledTotal-total).toFixed(2)}</h1>
+            <h1 className="text-2xl font-bold text-secondary">Grand Total: LKR. {total.toFixed(2)}</h1>
             <button onClick={onOrderCheckOutClick} className="bg-secondary text-accent p-2 rounded-lg w-[200px] hover:bg-light-secondary">Checkout</button>
         </div>
     )
